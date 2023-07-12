5ba784eca8a41c5e4d7327f5dcd98aec78d962b8
Agatha Odeh <agathaokofulds.com>
2023-07-12 02:27:44 +0100

    fix html validation error

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,12 +1,8 @@
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const weatherDesc = document.querySelector('#weather-desc');
+const weather = document.querySelector('#weather');
 
 const lat = 49.75;
 const lon = 6.64;
 const apiKey = "99fedeb9ab4dbde1f00c1d5fe29d45d0";
-// const apiKey = "d2bdb98b6d7ac1201a118ca4f3c694c1";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
@@ -27,10 +23,15 @@ async function apiFetch() {
   apiFetch();
 
   function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    weatherDesc.textContent = `${desc}`;
+
+    img.setAttribute('src', iconsrc);
+    img.setAttribute('alt', desc);
+    span.innerHTML = `${data.main.temp}&deg;F - ${desc}`;
+    weather.appendChild(img);
+    weather.appendChild(span);
   }
