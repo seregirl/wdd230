f66547b0602d9e31f1d7bc8608849ee4dd79832f
Agatha Odeh <agathaokofulds.com>
2023-07-12 02:01:31 +0100

    dynamic activity links

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,28 @@
 const baseURL = "https://seregirl.github.io/wdd230/";
 const linksURL = "https://seregirl.github.io/wdd230/data/links.json";
+const activities = document.getElementById("activity-links");
 
-// async function getLinks() {
-//     const response = await fetch(linksURL);
-//     const data = await response.json();
-//     displayLinks(data);
-//   }
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+  }
 
-// getLinks()
+getLinks()
+
+
+function displayLinks(weeks) {
+    weeks.forEach(wk => {
+        const li = document.createElement("li");
+        li.textContent = `${wk.week}: `;
+        wk.links.forEach((link, i) => {
+            const a = document.createElement("a");
+            a.textContent = link.title;
+            a.setAttribute('href', link.url);
+            if (i > 0) li.appendChild(document.createTextNode(' | '));
+            li.appendChild(a)
+        });
+
+        activities.appendChild(li);
+    });
+}
